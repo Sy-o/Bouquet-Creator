d513d06071d713515d454cd5114eef6f5605a834
Alice <fieryrobin@gmail.com>
2014-04-14 13:19:35 +0300

    add toString() and hashCode() to BuilderTO.java

--- src/bisnesslogic/to/BuilderTO.java
+++ src/bisnesslogic/to/BuilderTO.java
@@ -25,4 +25,27 @@ public class BuilderTO {
     public void setPacking(String packing){
         this.packing = packing;
     }
+
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result * this.packing.length();
+        result += result * prime + 172447 * ((this.flowers.size() > 7) ? 1 : 0);
+        return result;
+    }
+
+    @Override
+    public String toString(){
+        StringBuilder str = new StringBuilder();
+        String NEWLINE = System.getProperty("line.separator");
+        str.append(this.getClass().getName() + " Object {" + NEWLINE);
+        str.append("  flowers: ");
+        for(FlowerTO flower: flowers){
+            str.append(flower.getColor() + " " + flower.getFlower() + ", ");
+        }
+        str.append(NEWLINE);
+        str.append("  packing: " + packing + NEWLINE);
+        str.append("}");
+        return str.toString();
+    }
 }
