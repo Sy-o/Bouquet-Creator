54d3f924a7f190146ba2d0b350636e9681deadfe
Alice <fieryrobin@gmail.com>
2014-04-14 12:41:39 +0300

    add main

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+import view.View;
+/**
+ * Created by IntelliJ IDEA.
+ * User: Alice
+ * Date: Apr 9, 2014
+ * Time: 11:08:27 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Main {
+    public static void main(String[] args) {
+        View view = new View(System.out);
+        view.showMenu();
+    }
+        
+}
